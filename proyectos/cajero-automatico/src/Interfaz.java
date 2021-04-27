d8ea5bb88c5b887a0cb4761cca8d6281fbc13e9a
Alex Mendoza Ventura <alex.mendozvaventura@gmail.com>
2021-04-27 15:23:26 -0500

    Se creo proyecto cajero automático, se agrego la Clase interfaz v1

---
+++ proyectos/cajero-automatico/src/Interfaz.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/** v1
+ * Programa que emula la interfaz de un cajero automatico. El programa presentará las siguientes opciones al usuario:
+ *
+ * Eliga una de las siguientes opciones:
+ * Escriba 1 para consultar saldo
+ * Escriba 2 para ingresar dinero
+ * Escriba 3 para sacar dinero
+ * Escriba 4 para consultar los ultimos movimientos
+ * Una vez escrita la opción, pulse la tecla Enter
+ *
+ * Una vez el usuario ha elegido la opción, el programa le mostrará por pantalla el número correspondiente a la opción elegida
+ * */
+public class Interfaz {
+    public static void main(String[] args) {
+        System.out.println("Eliga una de las siguientes opciones:\n\tEscriba 1 para consultar saldo: \n\tEscriba 2 para ingresar dinero\n\tEscriba 3 para sacar dinero\n\tEscriba 4 para consultar los ultimos movimientos");
+        Scanner scanner = new Scanner(System.in);
+        int opcionSeleccionada = scanner.nextInt();
+        System.out.println("La opción que usted ha elegido es la " +opcionSeleccionada);
+    }
+}
