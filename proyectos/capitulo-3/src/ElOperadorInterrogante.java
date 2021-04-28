4802091336f2ad1631b908b4c7ae025039ea526f
Alex Mendoza Ventura <alex.mendozvaventura@gmail.com>
2021-04-28 17:42:26 -0500

    Se agrega la clase operador interrogante

---
+++ proyectos/capitulo-3/src/ElOperadorInterrogante.java
@@ -0,0 +1,31 @@
+/**
+ * Video26 - El operador interrogante
+ * */
+
+import java.util.Scanner;
+
+public class ElOperadorInterrogante {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce un número entero");
+        int dividendo = scanner.nextInt();
+        System.out.println("Introducción otro número entero");
+        int divisor = scanner.nextInt();
+        int resultado = 0;
+
+        //Uso de if tradicional
+        /*
+        if (divisor == 0){
+            resultado = 0;
+        }
+        else
+        {
+            resultado = dividendo/divisor;
+        }
+        */
+        //Uso de operador ternario
+        resultado = divisor == 0 ? 0 : dividendo/divisor;
+
+        System.out.println("El resultado de dividir " + dividendo + " entre " + divisor +" es "+resultado);
+    }
+}
