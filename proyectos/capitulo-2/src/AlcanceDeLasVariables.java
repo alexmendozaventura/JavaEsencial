43ea1b693595d4472f75b9b782e6b633e28ead35
Alex Mendoza Ventura <alex.mendozvaventura@gmail.com>
2021-04-28 10:55:33 -0500

    Se crea clase de Alcance de las variables

---
+++ proyectos/capitulo-2/src/AlcanceDeLasVariables.java
@@ -0,0 +1,17 @@
+/*
+* Video15 - El ciclo de vida de las variables
+* */
+public class AlcanceDeLasVariables {
+    public static void main(String[] args) {
+        int dias = 0 ;
+        while(dias < 30){
+            dias+=1;
+            int semanas = dias/7;
+            System.out.println("Han pasado " +dias + " días de lo que supone " +semanas + " semanas");
+            //int dias; //No se puede declarar una variable de bloque igual que una variable global
+        }
+
+        //int anios = semanas/52; //No se puede llamar a una variable de bloque interior desde un bloque exterior
+        int anios = dias/365;
+    }
+}
