27333086bfb90aa8f945e3fc94867e5c7eb5ff32
Alex Mendoza Ventura <alex.mendozvaventura@gmail.com>
2021-04-28 16:56:24 -0500

    Se creo el proyecto capitulo 3, se agrego la clase LaSentenciaIf

---
+++ proyectos/capitulo-3/src/LaSentenciaIf.java
@@ -0,0 +1,25 @@
+/**
+ * Video24 - La sentencia if
+ * */
+import java.util.Scanner;
+
+public class LaSentenciaIf {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Adivina el numero que estoy pensando del 1 al 10");
+        int numeroIntroducido = scanner.nextInt();
+        int numeroPensado = 5;
+
+        String color = "";
+
+        if (numeroIntroducido == numeroPensado ){
+            color = "\033[0;32m";
+            System.out.println(color+"¡Enhorabuena!");
+        }
+        else{
+            color = "\033[0;31m";
+            System.out.println(color+"Lo sentimos el número que estaba pensando es " +numeroPensado);
+
+        }
+    }
+}
