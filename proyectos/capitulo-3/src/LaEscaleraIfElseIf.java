110217a108e08ee3fce2509c185a43f11703a8d9
Alex Mendoza Ventura <alex.mendozvaventura@gmail.com>
2021-04-28 17:20:02 -0500

    Se creó la clase La escalera if-else-if

---
+++ proyectos/capitulo-3/src/LaEscaleraIfElseIf.java
@@ -0,0 +1,23 @@
+/**
+ * Video25 La escalera if-else-if
+ * */
+import java.util.Scanner;
+public class LaEscaleraIfElseIf {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escribe un número entero y te daré información sobre el");
+        int numero = scanner.nextInt();
+        if (numero < 3){
+            System.out.println("El número introducido es menor que 3");
+        }
+        else if (numero < 5){
+            System.out.println("El número que has introducido está entre 3 y " + numero);
+        }
+        else if (numero < 8){
+            System.out.println("El número que has introducido está entre 5 y " + numero);
+        }
+        else{
+            System.out.println("El número introducido es mayor o igual que 8");
+        }
+    }
+}
