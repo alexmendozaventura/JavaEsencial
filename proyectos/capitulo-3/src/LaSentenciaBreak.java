b781e10141e5687ac0a399ac3d0e2e55b71698c8
Alex Mendoza Ventura <alex.mendozvaventura@gmail.com>
2021-04-29 20:57:35 -0500

    Se creo la clase SentenciaBreak

---
+++ proyectos/capitulo-3/src/LaSentenciaBreak.java
@@ -0,0 +1,40 @@
+/**
+ * Video31 - La sentencia break
+ * */
+public class LaSentenciaBreak {
+    public static void main(String[] args) {
+
+        int numeroEncontrado ;
+        //Uso de break con ciclo for
+        System.out.println("**************Sentencia for********************");
+        for (numeroEncontrado = 2567;numeroEncontrado<=3642;numeroEncontrado++){
+            System.out.println("¿Será el número? " +numeroEncontrado);
+            if (numeroEncontrado%2==0 && numeroEncontrado%3==0 && numeroEncontrado%5==0){
+                break;
+            }
+        }
+        if(numeroEncontrado > 3642){
+            System.out.println("No hay ningún número 2567 y 3642 que sea multiplo de 3,2 y 5");
+        }
+        else {
+            System.out.println("El primer multiplo de 3,2 y 5 " +numeroEncontrado);
+        }
+
+        //Uso de break con ciclo while
+        System.out.println("**************Sentencia while********************");
+        numeroEncontrado = 2567;
+        while(numeroEncontrado<=3642){
+            System.out.println("¿Será el número? " +numeroEncontrado);
+            if (numeroEncontrado%2==0 && numeroEncontrado%3==0 && numeroEncontrado%5==0){
+                break;
+            }
+            numeroEncontrado++;
+        }
+        if(numeroEncontrado > 3642){
+            System.out.println("No hay ningún número 2567 y 3642 que sea multiplo de 3,2 y 5");
+        }
+        else {
+            System.out.println("El primer multiplo de 3,2 y 5 " +numeroEncontrado);
+        }
+    }
+}
