8b8214e504dba36374a04413461abad7bd3438f2
Alex Mendoza Ventura <alex.mendozvaventura@gmail.com>
2021-05-03 17:28:49 -0500

    Se agrega ejemplo de clases abstractas

---
+++ proyectos/capitulo-7/src/Figura2D.java
@@ -0,0 +1,7 @@
+/**
+ * Video68 - Clases abstractas
+ * */
+public abstract class Figura2D {
+    public abstract double obtenerArea();
+    public abstract double obtenerPerimetro();
+}
---
+++ proyectos/capitulo-7/src/Rectangulo.java
@@ -0,0 +1,21 @@
+/**
+ * Video68 - Clases abstractas
+ * */
+public class Rectangulo extends Figura2D{
+    private double anchura;
+    private double altura;
+
+    public Rectangulo(double anchura, double altura){
+        this.anchura = anchura;
+        this.altura = altura;
+    }
+    @Override
+    public double obtenerArea() {
+        return anchura * altura;
+    }
+
+    @Override
+    public double obtenerPerimetro() {
+        return 2 * anchura + 2 * altura ;
+    }
+}
